c64b7a8850825c07278c255f8b2cb0269b6190ea
Nick Rodriguez <endotnick@gmail.com>
2017-07-26 22:54:07 +0000

    add routing (WIP)
    TODO: https://angular.io/tutorial/toh-pt5#style-the-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,36 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }            from '@angular/core';
+import { BrowserModule }       from '@angular/platform-browser';
+import { FormsModule }         from '@angular/forms';
+import { RouterModule }        from '@angular/router';
 
-import { AppComponent }  from './app.component';
-import {HeroDetailComponent } from './hero-detail.component';
+import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { AppRoutingModule }    from './app-routing.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [
+    AppComponent 
+  ],
+  providers: [
+    HeroService
+  ]
 })
+
+
+
 export class AppModule { }
